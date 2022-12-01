b60711fd3f69228dfb0fd31340d2c8a0cbeb68ec
Rifat <mdrifatahmed787@gmail.com>
2022-12-02 00:22:34 +0600

    login and signup

---
+++ src/Routes/routes.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../Pages/Shared/ErrorPage";
+import Login from "../Pages/Login/Login";
+import Signup from "../Pages/Login/Signup";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
+
+export default router;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCudqDszOy2Isp-dm2W6rYTuQQWVaAlIGs",
+  authDomain: "aircnc-e0673.firebaseapp.com",
+  projectId: "aircnc-e0673",
+  storageBucket: "aircnc-e0673.appspot.com",
+  messagingSenderId: "274385808785",
+  appId: "1:274385808785:web:bed0f86f27c4530006be30",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
